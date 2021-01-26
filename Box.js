2e49f2b309668f453043cc46f709cad8f421c1ea
Devansha-09 <74242812+Devansha-09@users.noreply.github.com>
2021-01-26 15:49:00 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -19,21 +19,21 @@ class Box{
         
       //console.log(this.body.speed);
 
-      if(this.body.speed > 3){
-          var angle = this.body.angle;
-          var pos = this.body.position;
-          push();
-          translate(pos.x,pos.y);
-          rotate(angle);
-          rectMode(CENTER);
-          rect(0, 0, this.width, this.height);
-          pop();   
-      } else{
-          World.remove(world, this.body);
-          push();
-          this.visibility = this.visibility -5;
-          pop();
-      }
+//       if(this.body.speed > 3){
+//           var angle = this.body.angle;
+//           var pos = this.body.position;
+//           push();
+//           translate(pos.x,pos.y);
+//           rotate(angle);
+//           rectMode(CENTER);
+//           rect(0, 0, this.width, this.height);
+//           pop();   
+//       } else{
+//           World.remove(world, this.body);
+//           push();
+//           this.visibility = this.visibility -5;
+//           pop();
+//       }
     }
 
 }
