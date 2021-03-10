dc0d1fa820f7c38b232a1a159dcfd300b1a62715
Devansha-09 <74242812+Devansha-09@users.noreply.github.com>
2021-03-10 17:16:32 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -2,8 +2,8 @@ class Box{
     constructor(x,y,width,height){
         var options = {
             restitution:0.8,
-            friction:1.0,
-            density:1.0
+//             friction:1.0,
+//             density:1.0
         }
         this.body = Bodies.rectangle(x,y,width,height,options);
         this.width = width;
@@ -12,22 +12,22 @@ class Box{
     }
 
     display(){
-      var pos = this.body.position;
-      rectMode(CENTER);
-      rect(pos.x, pos.y, this.width, this.height);
-      fill("white");
+//       var pos = this.body.position;
+//       rectMode(CENTER);
+//       rect(pos.x, pos.y, this.width, this.height);
+//       fill("white");
         
       //console.log(this.body.speed);
 
 //       if(this.body.speed > 3){
-//           var angle = this.body.angle;
-//           var pos = this.body.position;
-//           push();
-//           translate(pos.x,pos.y);
-//           rotate(angle);
-//           rectMode(CENTER);
-//           rect(0, 0, this.width, this.height);
-//           pop();   
+          var angle = this.body.angle;
+          var pos = this.body.position;
+          push();
+          translate(pos.x,pos.y);
+          rotate(angle);
+          rectMode(CENTER);
+          rect(0, 0, this.width, this.height);
+          pop();   
 //       } else{
 //           World.remove(world, this.body);
 //           push();
